c2eea42d33bce299ed236e54ee6370e2712aa5c6
RohanGorla <rohangorla2010@gmail.com>
2024-05-17 16:35:41 +0530

    Todo list version 1

---
+++ client/src/App.jsx
@@ -0,0 +1,172 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [render, setRender] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [done, setDone] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    async function getTodos() {
+      await axios
+        .get("http://localhost:3000/")
+        .then((response) => {
+          console.log(response.data);
+          setTodos(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      await axios
+        .get("http://localhost:3000/done")
+        .then((response) => {
+          console.log(response.data);
+          setDone(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    getTodos();
+  }, [render]);
+
+  async function addTodo(e) {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:3000/add", {
+        todo_title: title,
+        todo_content: content,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setTitle("");
+    setContent("");
+    setRender(!render);
+  }
+
+  async function finishTodo(id) {
+    await axios
+      .post("http://localhost:3000/done", {
+        todo_id: id,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setRender(!render);
+  }
+
+  async function undoTodo(id) {
+    await axios
+      .post("http://localhost:3000/undo", { todo_id: id })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setRender(!render);
+  }
+
+  async function deleteTodo(id) {
+    await axios
+      .delete("http://localhost:3000/delete", { data: { id } })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setRender(!render);
+  }
+
+  return (
+    <>
+      <h1>ROHAN TODO LIST</h1>
+      <form onSubmit={addTodo} className="todo-form">
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+          placeholder="Enter title..."
+        ></input>
+        <label htmlFor="todo">Todo:</label>
+        <input
+          type="text"
+          id="todo"
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+          value={content}
+          placeholder="Enter todo..."
+        ></input>
+        <button type="submit">ADD</button>
+      </form>
+      <h2>{todos.length ? "TODOS:" : "YOU HAVE NOTHING-TODO"}</h2>
+      <div className="todos">
+        {todos.map((todo) => {
+          return (
+            <div className="todo-item" key={todo.id}>
+              <h3>{todo.title}</h3>
+              <p>{todo.content}</p>
+              <button
+                onClick={() => {
+                  finishTodo(todo.id);
+                }}
+              >
+                Done
+              </button>
+              <button
+                onClick={() => {
+                  deleteTodo(todo.id);
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+        <h2>{done.length ? "FINISHED TODOS:" : null}</h2>
+        <div className="finished">
+          {done.map((todo) => {
+            return (
+              <div key={todo.id}>
+                <h3>{todo.title}</h3>
+                <p>{todo.content}</p>
+                <button
+                  onClick={() => {
+                    undoTodo(todo.id);
+                  }}
+                >
+                  Undo
+                </button>
+                <button
+                  onClick={() => {
+                    deleteTodo(todo.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,79 @@
+import "dotenv/config";
+import express, { response } from "express";
+import mysql from "mysql2";
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8080;
+
+/* THIS IS FOR PRODUCTION PURPOSE */
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
+/* THIS IS FOR DEVELOPMENT PURPOSE */
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "r0hanr0n",
+  database: "serverone",
+  port: 3306,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connected");
+  }
+});
+
+app.get("/", (req, res) => {
+  db.query('select * from todos where done = "no"', (err, data) => {
+    if (err) return res.send(err);
+    return res.send(data);
+  });
+});
+
+app.get("/done", (req, res)=>{
+    db.query('select * from todos where done = "yes"', (err, data)=>{
+        if (err) return res.send(err);
+        return res.send(data);
+    })
+});
+
+app.post('/add', (req, res)=>{
+    let sql = "insert into todos (title, content, done) values ?";
+    let values = [[req.body.todo_title, req.body.todo_content, 'no']];
+    db.query(sql, [values]);
+    res.send("data inserted");
+});
+
+app.post('/done', (req, res)=>{
+    db.query("update todos set done = 'yes' WHERE id = ?", [req.body.todo_id]);
+    res.send("Update done");
+});
+
+app.post('/undo', (req, res)=>{
+    db.query("update todos set done = 'no' WHERE id = ?", [req.body.todo_id]);
+    res.send("Update undo");
+});
+
+app.delete('/delete', (req, res)=>{
+    db.query("delete from todos where id = ?", [req.body.id]);
+    res.send("Deleted todo");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at  http://localhost:${PORT}`);
+});
