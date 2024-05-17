71890fdbb3910982af7b8f0c486308f936cdca86
RohanGorla <rohangorla2010@gmail.com>
2024-05-18 02:33:58 +0530

    debugging env variables test 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   useEffect(() => {
     async function getTodos() {
-      let url = import.meta.env.VITE_BASE_URL + "/";
+      let url = `${import.meta.env.VITE_BASE_URL}/`;
       console.log(import.meta.env.VITE_BASE_URL);
       console.log(url);
       await axios
